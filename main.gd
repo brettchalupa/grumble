8bd13c055b196a647a4ce93c62e72834f03a07e9
Brett Chalupa <brettchalupa@gmail.com>
2023-02-16 16:41:35 -0500

    Re-render all controllers when connect/disconnect

--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var controller_scene: PackedScene
+var controller_children = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,8 +12,12 @@ func _on_joy_connection_changed(_id, _connected):
 	_render_controllers()
 
 func _render_controllers():
+	for child in controller_children:
+		remove_child(child)
+		
 	for id in Input.get_connected_joypads():
 		var controller = controller_scene.instantiate()
 		controller.jid = id
 		controller.position.x = id * 320
+		controller_children.append(controller)
 		add_child(controller)
